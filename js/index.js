f750406daec9d02a13594b9caf1fe1f82479b8b1
Souvik-Bhattacharyya <souvikbhattacharyya2001@gmail.com>
2021-04-25 00:49:20 +0530

    home page

---
+++ js/index.js
@@ -0,0 +1,56 @@
+// <!-- Scroll To Top Button -->
+
+mybutton = document.getElementById("myBtn");
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+// typedText
+const typedTextSpan = document.querySelector(".typed-text");
+const textArray = ["Website Design & Development","App Development", "Backend Operations For Night Shifts" ,"Andriod & iOS","Graphic Design","Digital Marketing"];
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 2000;
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type(){
+    if(charIndex < textArray[textArrayIndex].length){
+        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, typingDelay);
+    }
+    else{
+        setTimeout(erase, newTextDelay);
+    }
+};
+
+function erase(){
+    if(charIndex > 0){
+        typedTextSpan.textContent = textArray[textArrayIndex].substring(0,charIndex-1);
+        charIndex --;
+        setTimeout(erase , erasingDelay);
+    }
+    else{
+        textArrayIndex ++;
+        if(textArrayIndex >=textArray.length) textArrayIndex=0;
+        setTimeout(type, typingDelay + 1100);
+    }
+};
+
+document.addEventListener("DOMContentLoaded", function(){
+    setTimeout(type, newTextDelay + 250);
+});
