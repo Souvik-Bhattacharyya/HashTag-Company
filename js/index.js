7dc8a9cb02ec6cfde4a6908b6b8999ca7dfa20c2
Souvik-Bhattacharyya <souvikbhattacharyya2001@gmail.com>
2021-04-30 01:33:14 +0530

    About Us Page

--- js/index.js
+++ js/index.js
@@ -54,3 +54,11 @@ function erase(){
 document.addEventListener("DOMContentLoaded", function(){
     setTimeout(type, newTextDelay + 250);
 });
+
+
+// Svg Animated
+const logo = document.querySelectorAll("#top-svg path");
+
+for(let i = 0; i<logo.length; i++){
+    console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
+}
